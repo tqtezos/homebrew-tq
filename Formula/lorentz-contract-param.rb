412530cd3bd08795441e40deabfe836f82d741f7
Michael J. Klein <lambdamichael@gmail.com>
2019-09-25 14:55:36 -0400

    attempt to patch lorentz-contract-param formula

--- Formula/lorentz-contract-param.rb
+++ Formula/lorentz-contract-param.rb
@@ -11,13 +11,27 @@ class LorentzContractParam < Formula
   bottle do
     root_url "https://dl.bintray.com/michaeljklein/bottles-tq"
     cellar :any_skip_relocation
+    rebuild 1 if OS.linux?
     sha256 "8776b29565eeb1ef4841526c2371eb1ff7b968b0b75ed833e64aaf5cc556a94b" => :mojave
+    sha256 "76c96ae4b0b653b776acae40c711c127b5f426036b121c59813a2f86c1c31920" => :x86_64_linux
   end
 
-  depends_on "haskell-stack"
+  unless OS.mac?
+    resource "linux-stack" do
+      url "https://github.com/commercialhaskell/stack/releases/download/v2.1.3/stack-2.1.3-linux-x86_64-static.tar.gz"
+      sha256 "4e937a6ad7b5e352c5bd03aef29a753e9c4ca7e8ccc22deb5cd54019a8cf130c"
+    end
+  end
+
+  depends_on "haskell-stack" if OS.mac?
 
   def install
     ENV.deparallelize
+
+    if OS.linux?
+      (buildpath/"linux-stack").install resource("linux-stack")
+      ENV.append_path "PATH", "#{buildpath}/linux-stack"
+    end
 
     system "stack", "build"
 
@@ -37,5 +51,7 @@ class LorentzContractParam < Formula
 
   test do
     assert_predicate bin/"lorentz-contract-param", :exist?
+    assert_predicate bin/"lorentz-contract-storage", :exist?
+    assert_predicate bin/"lorentz-contracts", :exist?
   end
 end
