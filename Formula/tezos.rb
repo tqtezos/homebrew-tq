2b1874aee3df5d0abf409b7aea97bed6e44b126e
Michael J. Klein <lambdamichael@gmail.com>
2019-10-21 14:41:10 -0400

    linting, add mojave bottle sha

--- Formula/tezos.rb
+++ Formula/tezos.rb
@@ -8,25 +8,20 @@ class Tezos < Formula
   desc "Platform for distributed consensus with meta-consensus capability"
   homepage "https://gitlab.com/tezos/tezos"
 
-  url "https://gitlab.com/tezos/tezos.git",
-      :revision => "a7d357bb290eab43c92ec2245811e9af959d1d4c",
-      :branch   => "mainnet",
-      :shallow  => false
+  url "https://gitlab.com/tezos/tezos.git", :revision => "a7d357bb290eab43c92ec2245811e9af959d1d4c", :branch => "mainnet", :shallow  => false
   version "005_PsBabyM1"
 
   head "https://gitlab.com/tezos/tezos.git", :branch => "babylonnet"
 
-  # bottle do
-  #   root_url "https://dl.bintray.com/michaeljklein/bottles-tq"
-  #   cellar :any
-  #   rebuild 2 if OS.mac?
-  #   rebuild 3 if OS.linux?
-  #   sha256 "9f5eda43d650d7de741259c106c901c3a9b9364230f8606c0d584fa0b9ebd1db" => :mojave
-  #   sha256 "52bc29b5ad8b270227f276bda540d483af8f846cc7a88d7fcca34b215a85dfd0" => :x86_64_linux
-  # end
-    # cellar :any
-    # sha256 "2977a2d6578a57ef4e65aa577a1bb6aa5af54f2a02026e869e09289af9d78df5" => :mojave
-  # end
+# bottle do
+#   root_url "https://dl.bintray.com/michaeljklein/bottles-tq"
+#   cellar :any
+#   rebuild 2 if OS.mac?
+#   rebuild 3 if OS.linux?
+#   sha256 "9f5eda43d650d7de741259c106c901c3a9b9364230f8606c0d584fa0b9ebd1db" => :mojave
+#   sha256 "52bc29b5ad8b270227f276bda540d483af8f846cc7a88d7fcca34b215a85dfd0" => :x86_64_linux
+# end
+# sha256 "18513e5f094952b6c967e87abe75ae15f0995dae39caf4e84ce725bb02018be5" => :mojave
 
   depends_on "opam" => :build
 
