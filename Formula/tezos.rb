eb71df208def7e2c0f48822d2ac1e8109e837370
oper <oper@opers-MBP.lan1>
2020-06-04 08:30:37 -0400

    Move Tezos to the latest-release branch
    Also remove the botttle. I'm not intimately familiar with ruby, but
    that seems to work and generate the binaries.

--- Formula/tezos.rb
+++ Formula/tezos.rb
@@ -8,17 +8,20 @@ class Tezos < Formula
   desc "Platform for distributed consensus with meta-consensus capability"
   homepage "https://gitlab.com/tezos/tezos"
 
-  url "https://gitlab.com/tezos/tezos.git", :revision => "64c855499057fa84b41fcb9f3d7d72d4570db1a7", :branch => "mainnet", :shallow => false
-  version "006_PsCARTHA"
+  # url "https://gitlab.com/tezos/tezos.git", :revision => "64c855499057fa84b41fcb9f3d7d72d4570db1a7", :branch => "mainnet", :shallow => false
 
-  head "https://gitlab.com/tezos/tezos.git", :branch => "carthagenet"
+  url "https://gitlab.com/tezos/tezos.git", :revision => "51977265590ba5fbd166b921e265fa22bf9f66a6", :branch => "latest-release", :shallow => false
+  
+  version "007_multinetwork"
 
-  bottle do
-    root_url "https://dl.bintray.com/michaeljklein/bottles-tq"
-    cellar :any
-    sha256 "a804b682b3038693302b0116dbe8ddc8646c089456b0911318ae289022615626" => :catalina
-    sha256 "3ec8358445f73441f58fd11d4eeeb20eaedae749bb085df99cbc67ae99456ad9" => :x86_64_linux
-  end
+  head "https://gitlab.com/tezos/tezos.git", :branch => "v7-release"
+
+  # bottle do
+  #   root_url "https://dl.bintray.com/michaeljklein/bottles-tq"
+  #   cellar :any
+  #   sha256 "a804b682b3038693302b0116dbe8ddc8646c089456b0911318ae289022615626" => :catalina
+  #   sha256 "3ec8358445f73441f58fd11d4eeeb20eaedae749bb085df99cbc67ae99456ad9" => :x86_64_linux
+  # end
 
   depends_on "opam" => :build
 
